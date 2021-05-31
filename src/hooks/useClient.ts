a8ad7471ab8f0bab8768a2de19fa223df8ff5c28
Vladislav Polyakov <polrkov.tm@gmail.com>
2021-05-31 22:19:12 +0300

    feat: add builds files

--- src/hooks/useClient.ts
+++ src/hooks/useClient.ts
@@ -22,9 +22,9 @@ export type ISourcesResponse = ISuccessResponse & {
 } | IErrorResponse
 
 export interface GetSourcesRequestParams {
-  category?: 'business' | 'entertainment' | 'general' | 'health' | 'science' | 'sports' | 'technology'
-  language?: 'ar' | 'de' | 'en' | 'es' | 'fr' | 'he' | 'it' | 'nl' | 'no' | 'pt' | 'ru' | 'se' | 'ud' | 'zh'
-  country?: 'ae' | 'ar' | 'at' | 'au' | 'be' | 'bg' | 'br' | 'ca' | 'ch' | 'cn' | 'co' | 'cu' | 'cz' | 'de' | 'eg' | 'fr' | 'gb' | 'gr' | 'hk' | 'hu' | 'id' | 'ie' | 'il' | 'in' | 'it' | 'jp' | 'kr' | 'lt' | 'lv' | 'ma' | 'mx' | 'my' | 'ng' | 'nl' | 'no' | 'nz' | 'ph' | 'pl' | 'pt' | 'ro' | 'rs' | 'ru' | 'sa' | 'se' | 'sg' | 'si' | 'sk' | 'th' | 'tr' | 'tw' | 'ua' | 'us' | 've' | 'za'
+  category?: string
+  language?: string
+  country?: string
 }
 
 export interface GetArticlesRequestParams {
